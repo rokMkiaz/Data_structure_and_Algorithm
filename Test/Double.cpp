ac92aa6d3be7b7cf69cfc64b75269b6602895fc7
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2022-12-01 19:17:42 +0900

    22/12/01

---
+++ Test/Double.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<string>
+
+void set_proverb(std::string** q, int n);
+
+int main(void)
+{
+	std::string* s = NULL;
+	int n;
+
+
+	printf("몇번째 속담을 선택하시겠습니까?");
+	scanf_s("%d", &n);
+	set_proverb(&s, n);
+	printf_s("selected proverb = %s \n",s->c_str());
+
+	return 0;
+}
+
+void set_proverb(std::string** q, int n)
+{
+	static std::string array[10] = {
+	"Laughter is the best medicine."
+	,"Make hay while the sun shines."
+	,"Slowand steady wins the race."
+	,"Timeand tide wait for no man."
+	,"What goes around comes around."
+	,"Two heads are better than one."
+	,"Too many cooks spoil the broth."
+	,"All good things come to an end."
+	,"Actions speak louder than words."
+	,"Don't judge a book by its cover." };
+
+	*q = &array[n];
+}
+
