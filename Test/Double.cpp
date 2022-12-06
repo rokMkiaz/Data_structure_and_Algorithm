0852284a1a2cd4e97861fb4eca13fc3d0ac11242
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2022-12-06 14:31:12 +0900

    22/12/06

--- Test/Double.cpp
+++ Test/Double.cpp
@@ -19,17 +19,19 @@ int main(void)
 
 void set_proverb(const char** q, int  n)
 {
-	//static std::string array[10] = {
-	//"Laughter is the best medicine."
-	//,"Make hay while the sun shines."
-	//,"Slowand steady wins the race."
-	//,"Timeand tide wait for no man."
-	//,"What goes around comes around."
-	//,"Two heads are better than one."
-	//,"Too many cooks spoil the broth."
-	//,"All good things come to an end."
-	//,"Actions speak louder than words."
-	//,"Don't judge a book by its cover." };
+	static std::string array_2[10] = {
+	"Laughter is the best medicine."
+	,"Make hay while the sun shines."
+	,"Slowand steady wins the race."
+	,"Timeand tide wait for no man."
+	,"What goes around comes around."
+	,"Two heads are better than one."
+	,"Too many cooks spoil the broth."
+	,"All good things come to an end."
+	,"Actions speak louder than words."
+	,"Don't judge a book by its cover." };
+
+	
 
 	static const char* array[10] = {
 	"Laughter is the best medicine."
@@ -44,7 +46,7 @@ void set_proverb(const char** q, int  n)
 	,"Don't judge a book by its cover."
 	};
 
-
+	
 	*q = array[n];
 }
 
